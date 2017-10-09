a6e4eb5d4147822eb480ee9919edddcd59e68e0c
miaodx <miaodx@hotmail.com>
2017-10-09 19:32:32 +0800

    Add zed.bat for adding the necessary files to PATH.

---
+++ zed.bat
@@ -0,0 +1,3 @@
+set ZED_DIR=C:\Program Files (x86)\ZED SDK\
+set OpenCV_DIR=%ZED_DIR%\dependencies\opencv_3.1.0
+set path=%path%;%ZED_DIR%\bin;%ZED_DIR%\dependencies\freeglut_2.8\x64;%ZED_DIR%\dependencies\glew-1.12.0\x64;%ZED_DIR%\dependencies\opencv_3.1.0\x64\vc14\bin;
