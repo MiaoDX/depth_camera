7e5f6cdbadd34904c7e3257646232e97993144af
miaodx <miaodx@hotmail.com>
2018-01-06 19:57:20 +0800

    Add base class.

---
+++ zed_camera/CameraBase.py
@@ -0,0 +1,49 @@
+import abc
+import numpy as np
+
+class CameraCalibration(object):
+    def __init__(self, leftK = np.array([[320, 0, 320], [0, 320, 240], [0, 0, 1]]),
+                 rightK = np.array([[320, 0, 320], [0, 320, 240], [0, 0, 1]])):
+        self.__leftK = leftK
+        self.__rightK = rightK
+
+    def getK(self):
+        return self.__leftK.copy()
+
+    def getLeftK(self):
+        return self.__leftK.copy()
+
+    def getRightK(self):
+        return self.__rightK.copy()
+
+
+class CameraBase(object):
+    __metaclass__ = abc.ABCMeta
+
+    def __init__(self, cameraCalibration = CameraCalibration()) -> None:
+        self.cameraCalibration = cameraCalibration
+
+    def getCameraCalibration(self) -> CameraCalibration:
+        return self.cameraCalibration
+
+    @abc.abstractmethod
+    def open(self) -> bool:
+        return False
+
+    @abc.abstractmethod
+    def close(self) -> bool:
+        return False
+
+    @abc.abstractmethod
+    def getImage(self) -> (np.ndarray, np.ndarray):
+        rgb_image = np.ndarray(shape=(640, 480), dtype=np.uint8)
+        depth_image = np.ndarray(shape=(640, 480), dtype=np.uint8)
+        return rgb_image, depth_image
+
+    @abc.abstractmethod
+    def getParameters(self) -> dict:
+        return {}
+
+    @abc.abstractmethod
+    def setParameters(self, params : dict) -> bool:
+        return False
